3a68d4a9369b71c52f06b7642e24cc94c7d6be18
Michal Czaplinski <mmczaplinski@gmail.com>
2015-05-24 17:28:10 +0200

    lalala

---
+++ src/script.js
@@ -0,0 +1,25 @@
+var React = require('react')
+
+var Timer = React.createClass({
+  getInitialState: function() {
+    return {secondsElapsed: 0};
+  },
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <div className="well">
+        Seconds Elapsed: {this.state.secondsElapsed}
+      </div>
+    );
+  }
+});
+
+React.render(<Timer />, document.getElementById('content'));
