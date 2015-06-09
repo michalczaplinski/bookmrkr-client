cfa09b165141f09e12f2c72a4fa0f00405a751c1
Michal Czaplinski <mmczaplinski@gmail.com>
2015-06-09 18:42:50 +0200

    added jquery from the CDN and the js that uses it

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,44 @@
-var React = require('react')
+$(document).ready(function() {
 
-var Timer = React.createClass({
-  getInitialState: function() {
-    return {secondsElapsed: 0};
-  },
-  tick: function() {
-    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
-  },
-  componentDidMount: function() {
-    this.interval = setInterval(this.tick, 1000);
-  },
-  componentWillUnmount: function() {
-    clearInterval(this.interval);
-  },
-  render: function() {
-    return (
-      <div className="well">
-        Seconds Elapsed: {this.state.secondsElapsed}
-      </div>
-    );
-  }
+  // $('a.item').on("click", function(e) {
+  //   e.preventDefault();
+  //   $('.item').removeClass('active');
+  //   $(this).addClass('active');
+  // });
+  $('.right.menu.open').on("click",function(e){
+        e.preventDefault();
+    $('.ui.vertical.menu').toggle();
+  });
+
+  $('.ui.dropdown').dropdown();
+
 });
 
-React.render(<Timer />, document.getElementById('content'));
+// var React = require('react')
+
+// var Timer = React.createClass({
+//   getInitialState: function() {
+//     return {secondsElapsed: 0};
+//   },
+//   tick: function() {
+//     this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+//   },
+//   componentDidMount: function() {
+//     this.interval = setInterval(this.tick, 1000);
+//   },
+//   componentWillUnmount: function() {
+//     clearInterval(this.interval);
+//   },
+//   render: function() {
+//     return (
+//       <div className="well">
+//         Seconds Elapsed: {this.state.secondsElapsed}
+//       </div>
+//     );
+//   }
+// });
+
+// React.render(<Timer />, document.getElementById('content'));
 
 
 // var Item = React.createClass({
